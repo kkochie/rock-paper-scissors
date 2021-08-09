3da97d0a31c120e5455392d378245ef509dacc09
kkochie <kristinekochie@gmail.com>
2021-08-09 12:47:00 -0400

    Add points

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let playerSelection;
 let computerSelection;
+let computerScore = 0;
+let playerScore = 0;
 
 const array = ['rock', 'paper', 'scissors'];
 
@@ -10,26 +12,35 @@ function computerPlay() {
 function playRound(computerSelection,playerSelection) {
     playerSelection = prompt("Rock? Paper? Scissors?").toLowerCase();
     computerSelection = computerPlay();
-    if (computerSelection == playerSelection) {
-        return("Tie!");
-    } else if (
-        (computerSelection == "rock" && playerSelection == "scissors") ||
-        (computerSelection == "scissors" && playerSelection == "paper") ||
-        (computerSelection == "paper" && playerSelection == "rock")
-      ) {
-          return (`You lose! ${capitalize(computerSelection)} beats ${playerSelection}.`);
+  
+    if ((computerSelection === "rock" && playerSelection === "scissors") ||
+        (computerSelection === "scissors" && playerSelection === "paper") ||
+        (computerSelection === "paper" && playerSelection === "rock")) {
+        
+        computerScore ++;
+        return (`You lost this round! ${capitalize(computerSelection)} beats ${playerSelection}.`)
+        
+    } else if (computerSelection === playerSelection) {
+        return ("Tie!");      
+          
     } else {
-        return (`You win! ${capitalize(playerSelection)} beats ${computerSelection}.`);
+        playerScore ++;
+        return (`You win this round! ${capitalize(playerSelection)} beats ${computerSelection}.`);
     }
+
 }   
 
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-for (var i = 0; i < 5; i++) {
+function game() {
+    for (var i = 0; i < 5; i++)
+    
     console.log(playRound(computerSelection, playerSelection));
 }
+
+game();
 
 
 
